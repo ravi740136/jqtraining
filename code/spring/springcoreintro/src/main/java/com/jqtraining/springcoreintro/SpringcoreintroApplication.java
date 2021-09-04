f1883a9f062ff691389a59e17ebf6865c480d7da
ravi740136 <ravi740136@gmail.com>
2021-09-04 15:44:13 +0530

    adding spring intro project

---
+++ code/spring/springcoreintro/src/main/java/com/jqtraining/springcoreintro/SpringcoreintroApplication.java
@@ -0,0 +1,13 @@
+package com.jqtraining.springcoreintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreintroApplication.class, args);
+	}
+
+}
---
+++ code/spring/springcoreintro/src/test/java/com/jqtraining/springcoreintro/SpringcoreintroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jqtraining.springcoreintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreintroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

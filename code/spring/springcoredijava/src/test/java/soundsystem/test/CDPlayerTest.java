c7f97a73254dd539249243464593a8e26644aee2
ravi740136 <ravi740136@gmail.com>
2021-10-06 16:53:25 +0530

    to print the beans

--- code/spring/springcoredijava/src/test/java/soundsystem/test/CDPlayerTest.java
+++ code/spring/springcoredijava/src/test/java/soundsystem/test/CDPlayerTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -29,6 +31,8 @@ public class CDPlayerTest {
   @Autowired
 	PrintBeans pBeans;
 	
+  @Autowired
+  ApplicationContext context;
   
   @Test
 	public void validateCD() {
@@ -49,6 +53,10 @@ public class CDPlayerTest {
 	  System.out.println("from test: " + cd);
 		pBeans.print();
 		
+		String[] beans = context.getBeanNamesForAnnotation(Bean.class);
+		for (String bean : beans) {
+			System.out.println(bean);
+		}
   }
 
 }
